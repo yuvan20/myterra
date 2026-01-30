3606e5becbac22a894143fb2f0187c7ac57c20be
yuvan20 <115212455+yuvan20@users.noreply.github.com>
2026-01-30 12:16:29 +0530

    Add files via upload

---
+++ modules/ec2/main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "this" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  key_name      = var.key_name
+
+  tags = {
+    Name = "dev-ec2"
+  }
+}
---
+++ modules/ec2/variables.tf
@@ -0,0 +1,4 @@
+variable "ami_id" {}
+variable "instance_type" {}
+variable "subnet_id" {}
+variable "key_name" {}
